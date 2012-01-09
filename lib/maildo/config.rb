ebc0ac49a8bf229d449a3c0b64068e4cf4052db4
kbl <m.pietraszek@gmail.com>
2012-01-09 11:42:47 +0100

    working gmail configuration

--- lib/maildo/config.rb
+++ lib/maildo/config.rb
@@ -2,11 +2,19 @@ require 'maildo'
 
 module Maildo::Config
   Mail.defaults do
-    retriever_method :pop3, 
-                     :address    => 'pop.gmail.com',
-                     :port       => 995,
-                     :user_name  => 'rmutodo',
-                     :password   => ENV['RMU_TODO_PASS'],
-                     :enable_ssl => true
+    retriever_method :imap, 
+                   { address: 'imap.gmail.com',
+                     port: 993,
+                     user_name: 'rmutodo',
+                     password: ENV['MAILDO_PASS'],
+                     enable_ssl: true }
+    delivery_method :smtp, 
+                  { address: 'smtp.gmail.com',
+                    port: 587,
+                    domain: 'rmu-maildo.com',
+                    user_name: 'rmutodo@gmail.com',
+                    password: ENV['MAILDO_PASS'],
+                    authentication: 'plain',
+                    enable_starttls_auto: true } 
   end
 end
