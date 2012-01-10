f210fe5bf2d96cc592f559553f814e92b1482eee
kbl <m.pietraszek@gmail.com>
2012-01-10 18:42:26 +0100

    pending spec for done message

--- spec/lib/maildo/message/add_spec.rb
+++ spec/lib/maildo/message/add_spec.rb
@@ -5,14 +5,13 @@ module Maildo::Message
 
     after(:each) { empty_test_list_dir }
 
-    it 'should throw error on addint task to unsubscribed list' do
+    it 'should throw error on adding task to unsubscribed list' do
       lambda {
         Add.new('john@lenon.com', 'funny_list_id', 'task').execute
       }.should raise_error NotYetSubscribedError
     end
 
     context 'subscribed' do
-
       before(:each) { Subscribe.new('joe@smith.com', 'funny_list_id').execute }
 
       it 'should add tasks to list' do
@@ -21,7 +20,6 @@ module Maildo::Message
 
         tasks('funny_list_id').should == ['task', 'really hard task']
       end
-
     end
 
   end
--- spec/lib/maildo/message/done_spec.rb
+++ spec/lib/maildo/message/done_spec.rb
@@ -3,12 +3,34 @@ require 'spec_helper'
 module Maildo::Message
   describe Done do
 
+    SENDER = 'joe@smith.com'
+    LIST_ID = 'id7'
+
     after(:each) { empty_test_list_dir }
 
     it 'should throw error on changing task in unsubscribed list' do
       lambda {
-        Done.new('john@lenon.com', 'funny_list_id', 'task').execute
+        Done.new(SENDER, LIST_ID, 'task').execute
       }.should raise_error NotYetSubscribedError
+    end
+
+    context 'subscribed' do
+      before(:each) { Subscribe.new('joe@smith.com', 'id7').execute }
+
+      it 'should done task by 1 based index' do
+        pending
+        add_task('task')
+        add_task('to be done')
+        add_task('really hard task')
+
+        tasks(LIST_ID).should == ['task', 'to be done', 'really hard task']
+        Done.new(SENDER, LIST_ID, '2').execute
+        tasks(LIST_ID).should == ['task', 'really hard task']
+      end
+    end
+
+    def add_task(task)
+        Add.new(SENDER, LIST_ID, task).execute
     end
 
   end
