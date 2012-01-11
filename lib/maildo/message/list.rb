64e7fee05ce594f8d52546830da5f94ee59cb0e5
kbl <m.pietraszek@gmail.com>
2012-01-11 11:28:58 +0100

    response containig list of tasks

--- lib/maildo/message/list.rb
+++ lib/maildo/message/list.rb
@@ -9,7 +9,15 @@ module Maildo::Message
       response = super
       return response if response
 
-      tasks.tasks
+      t = tasks.tasks
+      Response.new("Todo list #{list_id}", response_body(t))
+    end
+
+    private
+
+    def response_body(tasks)
+      body = "List contains following tasks:\n\n"
+      body + tasks.map.each_with_index { |t, i| "#{i + 1}. #{t}" }.join("\n") + "\n"
     end
 
   end
--- spec/lib/maildo/message/list_spec.rb
+++ spec/lib/maildo/message/list_spec.rb
@@ -9,7 +9,7 @@ module Maildo::Message
     after(:each) { empty_test_list_dir }
 
     it 'should return access denied response' do
-      response = List.new(SENDER, LIST_ID).execute
+      response = list
       response.subject.should == 'Access denied'
       response.body.should match /Please subscribe to \[#{LIST_ID}\]/
     end
@@ -17,16 +17,31 @@ module Maildo::Message
     context 'subscribed' do
       before(:each) { Subscribe.new(SENDER, LIST_ID).execute }
 
-      it 'should list all tasks' do
-        add_task('task1')
-        add_task('super hard task')
+      it 'should return valid response' do
+        add_task('learn moonwalk')
+        add_task('do 100 pushups in 1 minute')
+        add_task('do 101 pushups in 2 minutes')
 
-        List.new(SENDER, LIST_ID).execute.should == ['task1', 'super hard task']
+        response = list
+        response.subject.should == "Todo list #{LIST_ID}"
+        response.body.should == 
+<<-STR
+List contains following tasks:
+
+1. learn moonwalk
+2. do 100 pushups in 1 minute
+3. do 101 pushups in 2 minutes
+STR
+
       end
     end
 
     def add_task(task)
       Add.new(SENDER, LIST_ID, task).execute
+    end
+
+    def list
+      List.new(SENDER, LIST_ID).execute
     end
 
   end
